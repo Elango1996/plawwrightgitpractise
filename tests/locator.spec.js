791a307d13b2e66f6be3f4fe91a613d82bbd40ff
elango1996 <elangovanjeeva10@gmail.com>
2026-08-21 17:40:55 +0530

    elango branch changed

--- tests/locator.spec.js
+++ tests/locator.spec.js
@@ -14,6 +14,7 @@ test('demo',async ({page})=>
     await page.getByPlaceholder("Current Address").fill("no 98 amman koil st saidapet rd vadapalani ch-26");
     console.log("poll scm done");
     console.log("elango branch done");
+    // cdone
 
-
+// super
 })
